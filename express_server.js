98d0277b455fc1f3e23eeb21ceb6a607ee50d0e4
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-22 01:49:19 -0700

    added header

--- express_server.js
+++ express_server.js
@@ -14,6 +14,11 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
@@ -25,15 +30,6 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
