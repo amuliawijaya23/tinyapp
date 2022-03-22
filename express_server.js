2c82a57bc668072ef681e87a34eb10a43035a35a
amuliawijaya23 <amuliawijaya@gmail.com>
2022-03-22 03:25:03 -0700

    added urls_new and function to generate random string

--- express_server.js
+++ express_server.js
@@ -1,22 +1,42 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+const generateRandomString = function() {
+  let randomChar = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2).toLocaleUpperCase();
+  let output = '';
+  for (let i = 0; i < 6; i++) {
+    output += randomChar.charAt(Math.floor(Math.random() * randomChar.length));
+  }
+  return output;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set('view engine', 'ejs');
-
 app.get('/urls', (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get('/', (req, res) => {
